2896d5bd23ff368d71c538117ab84438fd6a5fe3
youssef-rab1e <yoyomoh254@gmail.com>
2025-08-06 20:00:45 +0300

    Lexer Completed

--- lexer.py
+++ lexer.py
@@ -4,27 +4,32 @@ from dataclasses import dataclass
 
 #Token Types
 class TokenType(Enum):
-    NUMBER = "NUMBER"
-    IDENTIFIER = "IDENTIFIER"
-    STRING = "STRING"    
-    EQUALS = "EQUALS" #
-    COMA = "COMA" #
-    OPERATOR = "OPERATOR" #
-    KEYWORD = "KEYWORD"
-    BOOLEAN = "BOOLEAN" #
-    OPENPARENTHESIS = "OPENPARENTHESIS" #
-    CLOSEPARENTHESIS = "CLOSEPARENTHESIS" #
-    OPENBRACKET = "OPENBRACKET" #
-    CLOSEBRACKET = "CLOSEBRACKET" #
-    OPENCURLY = "OPENCURLY"    #
-    CLOSECURLY = "CLOSECURLY"    #
+    NUMBER = "NUMBER" 
+    IDENTIFIER = "IDENTIFIER"  
+    EQUALS = "EQUALS" 
+    COMA = "COMA" 
+    OPERATOR = "OPERATOR" 
+    BOOLEAN = "BOOLEAN" 
+    OPENPARENTHESIS = "OPENPARENTHESIS" 
+    CLOSEPARENTHESIS = "CLOSEPARENTHESIS" 
+    OPENBRACKET = "OPENBRACKET" 
+    CLOSEBRACKET = "CLOSEBRACKET" 
+    OPENCURLY = "OPENCURLY"    
+    CLOSECURLY = "CLOSECURLY" 
+    LETTOKEN = "LETTOKEN" 
+   
 
+KEYWORDS: dict[str, TokenType] = {
+    "let": TokenType.LETTOKEN,
+}
 
 @dataclass
 class Token:
     value: str
     type: TokenType
 
+def isskipper(src: str):
+    return src == " " | src == "\n" | src == "\t"
 
 def crt_token(value: str, type: TokenType):
     return {value , type}
@@ -75,7 +80,26 @@ def tokenize(SourceCode: str):
           token = crt_token(src.pop(0), TokenType.NUMBER)
           tokens.append(token)
         else :
-          # Here We Will Handle MultiChar tokens
-
-          pass
+          # Here We Will Handle MultiChar tokens Or Unkown Ones
+          if isnum(src[0]):
+            num = ""
+            while src.__len__ > 0 and isnum(src[0]):
+              num += src.pop(0)
+            tokens.append(crt_token(num, TokenType.NUMBER))
+          elif isalpha(src[0]):
+            identifier = ""
+            while src.__len__ > 0 and isalpha(src[0]):
+              identifier += src.pop(0)
+            # Check for reserved keywords
+            reserved = KEYWORDS.get(identifier)
+            if reserved ==  None:
+              tokens.append(crt_token(identifier, TokenType.IDENTIFIER))
+            else:
+              tokens.append(crt_token(identifier, reserved))
+          elif isskipper(src[0]):
+            src.pop(0)
+          else:
+            print(f"Unrecognized Character Found in Source: {src[0]}")
+            exit(1)
     return tokens
+
